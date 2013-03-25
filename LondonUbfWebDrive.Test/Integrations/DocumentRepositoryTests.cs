3cb362f2c968814572c836a494f989c7e91aec5b
Andrew Chaa <andrew.chaa@huddle.com>
2013-03-25 13:49:00 +0000

    Document name and fullname property tests

--- LondonUbfWebDrive.Test/Integrations/DocumentRepositoryTests.cs
+++ LondonUbfWebDrive.Test/Integrations/DocumentRepositoryTests.cs
@@ -8,10 +8,11 @@ using Machine.Specifications;
 
 namespace LondonUbfWebDrive.Test.Integrations
 {
-    public class FileSystemTests
+    public class DocumentRepositoryTests
     {
         protected static DirectoryInfo Directory;
         protected static IEnumerable<Document> Documents;
+        protected static Document Document;
         protected static IDocumentRepository Repository;
         protected static string MyDocument;
 
@@ -20,7 +21,7 @@ namespace LondonUbfWebDrive.Test.Integrations
     }
 
     [Subject(typeof(Document))]
-    public class When_it_reads_directories_in_the_given_directory : FileSystemTests
+    public class When_it_reads_directories_in_the_given_directory : DocumentRepositoryTests
     {
         private Establish context = () => Repository = new DocumentRepository(); 
         private Because it_reads_the_directory = () =>
@@ -32,13 +33,21 @@ namespace LondonUbfWebDrive.Test.Integrations
     }
 
     [Subject(typeof(Document))]
-    public class When_it_reads_files_in_the_given_directory : FileSystemTests
+    public class When_it_reads_files_in_the_given_directory : DocumentRepositoryTests
     {
         private Establish context = () => Repository = new DocumentRepository();
         private Because It_reads_the_files = () => Documents = Repository.Read(MyDocument);
 
         private It should_have_the_list_of_files = () => Documents.Where(d => !d.IsDirectory).ShouldNotBeEmpty();
+    }
 
+    [Subject(typeof(Document))]
+    public class When_it_reads_a_document : DocumentRepositoryTests
+    {
+        private Establish context = () => Repository = new DocumentRepository();
+        private Because It_reads_file_as_document = () => Document = Repository.Read(MyDocument).First();
 
+        private It should_have_document_name = () => Document.Name.ShouldNotBeEmpty();
+        private It should_have_document_full_name = () => Document.FullName.ShouldNotBeEmpty();
     }
 }
