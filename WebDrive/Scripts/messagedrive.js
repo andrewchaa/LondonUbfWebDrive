2d8a680776bf600422bbbb1ecc93dccf5f95899e
Andrew Chaa <andrew.chaa@yahoo.co.uk>
2014-04-21 10:25:05 +0100

    cleaned up message js

--- WebDrive/Scripts/messagedrive.js
+++ WebDrive/Scripts/messagedrive.js
@@ -2,8 +2,6 @@
     var self = this;
     self.breadcrumbs = ko.observableArray();
     self.documents = ko.observableArray();
-    self.recentDownloads = ko.observableArray();
-    self.filesUploaded = ko.observableArray();
     self.path = '';
 
     self.getPath = function() {
@@ -44,12 +42,6 @@
         });
     };
 
-    self.getPopularDownload = function() {
-        $.get('api/recentdownloads', function(data) {
-            self.recentDownloads(data);
-        });
-    };
-
     self.clickItem = function (item) {
         if (!!item.IsFolder) {
             self.list(item.FullName);
@@ -69,16 +61,6 @@
         $.post('api/metadata', "=" + ko.toJSON(item));
     };
 
-    self.displayUploadDoneFor = function(file) {
-        console.log(file);
-        self.filesUploaded.push(file);
-        self.list(self.path);
-    };
-
-    self.clearUploadDone = function() {
-        self.filesUploaded.removeAll();
-    };
-
 };
 
 $(function () {
@@ -88,75 +70,7 @@ $(function () {
 
     viewModel.list();
     viewModel.getBreadcrumbs('/');
-    viewModel.getPopularDownload();
     
-    $('#btnUpload').click(function () {
-        $('#dropbox').modal();
-    });
-
-    var dropbox = $('#dropbox');
-
-    dropbox.filedrop({
-        paramname: 'uploadFiles',
-        maxfiles: 5,
-        maxfilesize: 2, // in mb
-        url: '/api/messagedocuments',
-        data: {
-            selectedDir: function () {
-                return viewModel.getPath();
-            }
-        },
-        uploadFinished: function (i, file, response) {
-            $('#dropbox').modal('hide');
-            $('#msgUploadDone').fadeIn();
-
-            viewModel.displayUploadDoneFor(file);
-            setTimeout(function () {
-                $('#msgUploadDone').fadeOut();
-                viewModel.clearUploadDone();
-            }, 3000);
-
-        },
-
-        error: function (err, file) {
-            switch (err) {
-                case 'BrowserNotSupported':
-                    showMessage('Your browser does not support HTML5 file uploads!');
-                    break;
-                case 'TooManyFiles':
-                    alert('Too many files! Please select 5 at most!');
-                    break;
-                case 'FileTooLarge':
-                    alert(file.name + ' is too large! Please upload files up to 2mb.');
-                    break;
-                default:
-                    break;
-            }
-        },
-
-        // Called before each upload is started
-        beforeEach: function (file) {
-            path = $('#btnUpload').attr('data-path');
-            console.log(path);
-        },
-
-        uploadStarted: function (i, file, len) {
-            //                createImage(file);
-        },
-
-        progressUpdated: function (i, file, progress) {
-            //                $.data(file).find('.progress').width(progress);
-        }
-
-    });
-
-    var template = '...';
-
-
-    function showMessage(msg) {
-        //            message.html(msg);
-    }
-
 
 });
 
